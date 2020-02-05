2c73b7a72182b848b690835c7b1475837d068e68
schultz1 <schultz1@iastate.edu>
2020-02-05 16:48:51 -0600

    add justins code for sockets

--- cowchips-admin-master/server.js
+++ cowchips-admin-master/server.js
@@ -4,11 +4,50 @@ const path = require('path')
 const app = express()
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 
+
 app.use(redirectToHTTPS([/localhost:(\d{4})/]))
 app.use(serveStatic(path.join(__dirname, '/dist')))
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/dist/index.html')
 })
 const port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+const server = app.listen(`${port}`, function () {
+  console.log(`Server started on port ${port}`)
+})
+
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+  console.log("we in bois")
+})
+
+io.on("connection", socket => {
+  setInterval(() => {
+    socket.broadcast.emit("newdata", "hi")
+  }, 5000)
+});
+
+//THIS IS TESTING WITH FRONT END BACK END SOCKET IMPLEMENTATION
+
+// const ioc = require('socket.io-client')
+// var socket = ioc.connect('http://localhost:4446')
+// socket.on("newdata", (cooldata) => {
+//   console.log(cooldata);
+// })
+
+// const server = app.listen(4445, () => {
+//   console.log("websocket connected front to back")
+// })
+// const io = require('socket.io')(server)
+//
+// io.on("newdata", socket => {
+//   console.log(socket);
+// });
+
+// var socket = io.connect('http://localhost:4444')
+// socket.on("newdata", (cooldata) => {
+//   console.log(cooldata);
+// })
+//
+
+// console.log('server started ' + port)
+
