51354f272ae5ef961dad33ca57312ae17ff99299
schultz1 <schultz1@iastate.edu>
2020-02-14 12:07:14 -0600

    demo how to call another vue chart

--- cowchips-admin-master/src/views/games/GameAnalytics.vue
+++ cowchips-admin-master/src/views/games/GameAnalytics.vue
@@ -1,7 +1,20 @@
 <template>
-  <p>GameAnalytics.vue yay</p>
-  <!-- test url to inject: 
+  <div>
+    <p>GameAnalytics.vue yayy</p>
+    <!-- test url to inject:
       http://localhost:8080/games/analytics/5c7c4a32c0cdf2591d1b4b59
       -->
+    <card-line1-chart-example chartId="card-chart-01" class="chart-wrapper px-3" style="height:70px;" :height="70" />
+  </div>
 </template>
 
+<script>
+  import CardLine1ChartExample from '../dashboard/CardLine1ChartExample'
+
+  export default {
+    name: 'gameAnalytics',
+    components: {
+      CardLine1ChartExample,
+    },
+  }
+</script>
