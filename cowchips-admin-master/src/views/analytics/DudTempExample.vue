313b345dd018d0fdd1437d480e35cdd03753d795
schultz1 <schultz1@iastate.edu>
2020-02-14 12:23:09 -0600

    demonstrate a more custom example

---
+++ cowchips-admin-master/src/views/analytics/DudTempExample.vue
@@ -0,0 +1,39 @@
+<script>
+import { Bar } from 'vue-chartjs'
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips'
+
+export default {
+  extends: Bar,
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart(
+      {
+        labels: ['Cake Present', 'Cake Required'],
+        datasets: [
+          {
+            label: 'Cakes',
+            backgroundColor: '#f87979',
+            data: [40, 200]
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: true,
+        tooltips: {
+          enabled: false,
+          custom: CustomTooltips,
+          intersect: true,
+          mode: 'index',
+          position: 'nearest',
+          callbacks: {
+            labelColor: function (tooltipItem, chart) {
+              return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].backgroundColor }
+            }
+          }
+        }
+      }
+    )
+  }
+}
+</script>
--- cowchips-admin-master/src/views/games/GameAnalytics.vue
+++ cowchips-admin-master/src/views/games/GameAnalytics.vue
@@ -4,17 +4,17 @@
     <!-- test url to inject:
       http://localhost:8080/games/analytics/5c7c4a32c0cdf2591d1b4b59
       -->
-    <card-line1-chart-example chartId="card-chart-01" class="chart-wrapper px-3" style="height:70px;" :height="70" />
+    <dud-temp-example chartId="card-chart-01" class="chart-wrapper px-3" style="height:70px;" :height="70" />
   </div>
 </template>
 
 <script>
-  import CardLine1ChartExample from '../dashboard/CardLine1ChartExample'
+  import DudTempExample from '../analytics/DudTempExample'
 
   export default {
     name: 'gameAnalytics',
     components: {
-      CardLine1ChartExample,
+      DudTempExample,
     },
   }
 </script>
